4761a56a6dece94e6e41ecbe31995829407b372e
simhl1123 <77651941+simhl1123@users.noreply.github.com>
2023-12-01 10:40:16 +0800

    Update Set.java

--- src/adt/Set.java
+++ src/adt/Set.java
@@ -8,6 +8,55 @@ package adt;
  *
  * @author fongj
  */
-public class Set {
+public class Set<T> implements SetInterface<T>{
+    T[] setArray;
+    int numberOfElements;
+    private static final int DEFAULT_INITIAL_CAPACITY = 25;
     
+    public Set(){
+        this(DEFAULT_INITIAL_CAPACITY);
+    }
+    
+    public Set(int initialCapacity){
+        numberOfElements=0;
+        setArray = (T[]) new Object[initialCapacity];
+    }
+    
+    @Override
+    public boolean add(T newEntry){
+        for(int i=0; i<numberOfElements; ++i){
+            if(setArray[i].equals(newEntry)){
+                return false;
+            }
+        }
+        
+        if(isArrayFull()){
+            doubleArray();
+        }
+        setArray[numberOfElements] = newEntry;
+        numberOfElements++;
+        return true;
+    }
+    public boolean isArrayFull(){
+        return numberOfElements == setArray.length;
+    }
+    public void doubleArray(){
+        T[] oldArray = setArray;
+        int oldSize = oldArray.length;
+        
+        setArray =(T[]) new Object[2 * oldSize];
+        for (int index=0; index<oldSize; index++){
+            setArray[index] = oldArray[index];
+        }
+        
+}
+    
+    @Override
+    public String toString(){
+        String outputStr = "";
+        for (int i = 0; i<numberOfElements; ++i ){
+            outputStr += setArray[i] + "\n";
+        }
+        return outputStr;
+    }
 }
