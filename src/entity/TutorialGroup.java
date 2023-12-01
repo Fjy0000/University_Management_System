061146d5d1024ac91319ac6167770b7338bba9f5
simhl1123 <77651941+simhl1123@users.noreply.github.com>
2023-12-01 10:41:38 +0800

    Update TutorialGroup.java

--- src/entity/TutorialGroup.java
+++ src/entity/TutorialGroup.java
@@ -8,6 +8,54 @@ package entity;
  *
  * @author hongli
  */
-public class TutorialGroup {
+import adt.Set;
+import adt.SetInterface;
+import java.io.Serializable;
+
+/**
+ *
+ * @author 60111
+ */
+public class TutorialGroup implements Serializable{
+    private String groupName;
+    private static int groupNum = 1;
+    private boolean assigned;
     
+    public TutorialGroup() {
+    }
+
+    public TutorialGroup(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public static int getGroupNum() {
+        return groupNum;
+    }
+
+    public static void setGroupNum(int groupNum) {
+        TutorialGroup.groupNum = groupNum;
+    }
+
+    public boolean isAssigned() {
+        return assigned;
+    }
+
+    public void setAssigned(boolean assigned) {
+        this.assigned = assigned;
+    }
+    
+    @Override
+    public String toString() {
+        return "Tutorial Group\n" 
+                + "Group Name: " + groupName
+                + "\nIs Assigned: " + assigned;
+    }
 }
