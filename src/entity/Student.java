d1cbad8d2edd28085c4863ebbfd997b86e5ea30f
simhl1123 <77651941+simhl1123@users.noreply.github.com>
2023-12-04 23:32:08 +0800

    Update Student.java

--- src/entity/Student.java
+++ src/entity/Student.java
@@ -4,10 +4,52 @@
  */
 package entity;
 
+import java.util.Objects;
+
 /**
  *
- * @author fongj
+ * @author 60111
  */
 public class Student {
-    
+    private int id;
+    private String name;
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name=" + name + '}';
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Student student = (Student) obj;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
 }
