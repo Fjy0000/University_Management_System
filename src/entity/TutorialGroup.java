89a33c7e4865d51548dfbd446c781d98f2223480
simhl1123 <77651941+simhl1123@users.noreply.github.com>
2023-12-12 00:44:03 +0800

    Update TutorialGroup.java

--- src/entity/TutorialGroup.java
+++ src/entity/TutorialGroup.java
@@ -12,14 +12,13 @@ import java.util.Iterator;
  *
  * @author 60111
  */
-public class TutorialGroup{
+public class TutorialGroup implements Comparable<TutorialGroup>{
     private String groupName;
     private static int groupNum = 1;
     private boolean assigned;
     private SetInterface<Student> students = new arraySet<>();
     
     public TutorialGroup() {
-        this.students = new arraySet<>(); // Initialize the set of students
         this.groupName = "G" + groupNum;
         assigned = false;
         groupNum++;
@@ -31,19 +30,62 @@ public class TutorialGroup{
         assigned = false;
         groupNum++;
     }
-    public void addStudent(Student student) {
+   public void addStudent(Student student) {
+        if (student != null) {
             students.add(student);
         }
+    }
 
-    // Method to list all students in the tutorial group
-    public void listStudents() {
-        System.out.println("Students in Tutorial Group " + groupName + ":");
-        Iterator<Student> iterator = students.getIterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
+//    public void addStudents(SetInterface<Student> studentSet) {
+//        students.addAll(studentSet);
+//    }
+    
+    public void removeStudent(Student student) {
+        if (student != null) {
+            students.remove(student);
         }
     }
     
+    public boolean containsStudent(Student student) {
+        return students.contains(student);
+    }
+    public SetInterface<Student> getStudentsSet() {
+        return students;
+    }
+    // Method to list all students in the tutorial group
+//    public void listStudents() {
+//        System.out.println("Students in Tutorial Group " + groupName + ":");
+//        Iterator<Student> iterator = students.getIterator();
+//        while (iterator.hasNext()) {
+//            System.out.println(iterator.next());
+//        }
+//    }
+    public void listStudents() {
+        System.out.println("Students in Tutorial Group " + groupName + ":");
+            if (students.isEmpty()) {
+                System.out.println("No students in this group.");
+            } else {
+                Iterator<Student> iterator = students.getIterator();
+                while (iterator.hasNext()) {
+                    System.out.println(iterator.next());
+                }
+            }
+    }
+    
+//    public Student findStudentById(String studentId) {
+//        Iterator<Student> iterator = students.getIterator();
+//        while (iterator.hasNext()) {
+//            Student student = iterator.next();
+//            if (student.getId().equals(studentId)) {
+//                return student;
+//            }
+//        }
+//        return null; // Student not found in the group
+//    }
+    
+    public int getTotalEntries() {
+        return students.getTotalEntries();
+    }
 
     public String getGroupName() {
         return groupName;
@@ -67,6 +109,10 @@ public class TutorialGroup{
 
     public void setAssigned(boolean assigned) {
         this.assigned = assigned;
+    }
+    @Override
+    public int compareTo(TutorialGroup o) {
+      return groupName.compareTo(o.groupName);
     }
     
     @Override
