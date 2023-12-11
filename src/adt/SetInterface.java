3dc94eb659b685f508796a7597bade395823ae7f
simhl1123 <77651941+simhl1123@users.noreply.github.com>
2023-12-12 00:42:12 +0800

    Update SetInterface.java

--- src/adt/SetInterface.java
+++ src/adt/SetInterface.java
@@ -4,13 +4,19 @@
  */
 package adt;
 
+import java.util.Iterator;
+
 /**
  *
- * @author fongj
+ * @author 60111
  */
 public interface SetInterface<T> {
     public boolean add(T newElement);
+//    public boolean addAll(SetInterface<T> otherSet);
+    public boolean remove(T newElement);
     public boolean contains(T anEntry);
     public Iterator<T> getIterator();
     public int getTotalEntries();
+    public boolean isEmpty();
+    public void union(SetInterface anotherSet);
 }
